1937dbf78450fee5d7e81da21aeabae5ec1d523c
Sushmitha devadiga <sushmithadevadiga45@gmail.com>
2023-02-14 10:59:47 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Image from "./assets/peacock.jpg";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const x = 10;
+  var text;
+  if (x === 1) {
+  text = "Equal";
+  } else {
+    text = "Not Equal"; 
+  }
+
+  return ( <div className="App" >
+    <h1>Hello world</h1> 
+    <p>this is paragraph</p>
+    <h3>{5+50}</h3>
+    <img src ={Image} alt="pic" width="10px" />
+    <h2>{text}</h2>
+    <h2>{x >= 10 ? "greater" : "lesser"} </h2>
     </div>
-  );
+      );
+    
 }
 
 export default App;
